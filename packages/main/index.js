12989bdc5ea4098ba4a16a4214fdaeb7f1c3956e
Logan Powell <loganp@tepper.cmu.edu>
2019-08-16 13:11:31 -0400

    fixes main

--- packages/main/index.js
+++ packages/main/index.js
@@ -1,5 +1,5 @@
 const co = require('co')
-const { Channel } = require('core-async')
+const { Channel, alts } = require('core-async')
 const r = require('ramda')
 
 /**
@@ -8,17 +8,17 @@ const r = require('ramda')
     used to resolve to a value instead of a channel or trigger an error function 
     (mutually exclusive)
  */
-const breaker = (msg = e => console.log(`breaker tripped! ${e}`), _E_) => 
-  _I_ => {
-  const [v,chan] = yield alts([_E_, _I_])
+const breaker = (msg = e => console.log(`breaker tripped! ${e}`), _E_) => _I_ =>
+  co(function*() {
+    const [v, chan] = yield alts([_E_, _I_])
 
-  if (chan == _E_) {
-    return msg(v)
-    _I_.close()
-  } else {
-    return v
-  }
-}
+    if (chan == _E_) {
+      return msg(v)
+      _I_.close()
+    } else {
+      return v
+    }
+  })
 
 /**
  * apply a function to a value between other channel returning `plug`s
@@ -26,15 +26,14 @@ const breaker = (msg = e => console.log(`breaker tripped! ${e}`), _E_) =>
     a plug that takes a synchronous function and applies it to the async
     result and then passes it on
  */
-const tapWith = fn => 
-  _E_ => 
-  _I_ => {
+const tapWith = fn => _E_ => _I_ =>
+  co(function*() {
     let x = yield _I_.take()
     let y = fn(x)
     let _O_ = new Channel()
     _O_.put(y)
     return _O_
-  }
+  })
 
 /**
  * race between multiple plugs
@@ -42,20 +41,21 @@ const tapWith = fn =>
     for each plug and uses alts to choose the first of the chans to 
     avail a successful take() 
  */
-const race = (...pN) => 
-  _I_ => 
-  co(function *() {
+const race = (...pN) => _I_ =>
+  co(function*() {
     const I = yield _I_.take()
     const _O_ = new Channel()
 
-    r.map(p => {
-      const _e_ = new Channel()
-      const _i_ = new Channel()
-      _i_.put(I)
-      const _o_ = yield p(_e_)(_i_)
-      const o = yield _o_.take()
-      _O_.put(o)
-    })(pN)
+    r.map(p =>
+      co(function*() {
+        const _e_ = new Channel()
+        const _i_ = new Channel()
+        _i_.put(I)
+        const _o_ = yield p(_e_)(_i_)
+        const o = yield _o_.take()
+        _O_.put(o)
+      })
+    )(pN)
 
     const r = yield _O_.take()
     return r
@@ -66,25 +66,25 @@ const race = (...pN) =>
     together in order. Requires three instantiated channels
     the signature of each fn: see example 'plug' below
  */
-const run = (...pN) => 
-  _E_ => 
-  _I_ => 
-  co(function *() {
-  const I = yield _I_.take()
+const run = (...pN) => _E_ => _I_ =>
+  co(function*() {
+    const I = yield _I_.take()
 
-  const _O_ = yield r.pipe(p => co(function *(){
-    const _i_ = new Channel()
-    _i_.put(I)
-    const _o_ = yield p(_E_)(_i_)
-    return _o_
-  })(pN))
+    const _O_ = yield r.pipe(p =>
+      co(function*() {
+        const _i_ = new Channel()
+        _i_.put(I)
+        const _o_ = yield p(_E_)(_i_)
+        return _o_
+      })(pN)
+    )
 
-  return _O_
-})
+    return _O_
+  })
 
 module.exports = {
   run,
   race,
   tapWith,
-  breaker
+  breaker,
 }
